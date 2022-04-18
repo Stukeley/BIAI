63e7cac5d48d94e0fc15bca77725e82056bbe43c
Rafał Klinowski <klinowskirafal@gmail.com>
2022-04-18 14:15:11 +0200

    Add project card. Add experimental saving and loading models to/from a file. Add a function calculating the average size (width, height) of used images, and alter the model based on that average.

--- file_reorganisation.py
+++ file_reorganisation.py
@@ -1,5 +1,7 @@
 ﻿import os
+from fnmatch import fnmatch
 
+from PIL import Image
 
 # Helper function that groups all the images into separate folders based on the cat/dog breed name
 # Example: By default, all the images are in the /images folder.
@@ -30,3 +32,57 @@ def create_folders_and_move_image_files():
 
         # Move the file to the new folder
         os.rename(images_dir + "/" + file_name, images_dir + "/" + split_name + "/" + file_name)
+
+# Helper function that calculates the minimum, maximum and average size of files in the images' directory
+# The results will be used to resize the images to a uniform size
+def calculate_min_max_avg_image_size():
+    
+    # Get all files from the images directory as well as the subdirectories
+    # Only list the files that end with .jpg
+    file_list = []
+
+    for (dirpath, dirnames, filenames) in os.walk(os.path.dirname(os.path.realpath(__file__)) + '/images'):
+        file_list += [os.path.join(dirpath, file) for file in filenames if file.endswith('.jpg')]
+    
+    max_width = 0
+    max_height = 0
+    min_width = 0
+    min_height = 0
+    sum_width = 0
+    sum_height = 0
+    count = 0
+    
+    # Get info of each file from file_list
+    for file_name in file_list:
+        img = Image.open(file_name)
+        width, height = img.size
+        
+        # Update max and min width and height
+        if width > max_width:
+            max_width = width
+        if width < min_width:
+            min_width = width
+        if height > max_height:
+            max_height = height
+        if height < min_height:
+            min_height = height
+            
+        # Update sum of width and height
+        sum_width += width
+        sum_height += height
+        count += 1
+        
+    # Calculate average width and height
+    avg_width = sum_width / count
+    avg_height = sum_height / count
+    
+    print("Min width: " + str(min_width) + ", Max width: " + str(max_width) + ", Avg width: " + str(avg_width))
+    print("Min height: " + str(min_height) + ", Max height: " + str(max_height) + ", Avg height: " + str(avg_height))
+    
+    # Results:
+    # max_width = 3264
+    # max_height = 2606
+    # min_width = 0
+    # min_height = 0
+    # avg_width = 437
+    # avg_height = 391
--- main.py
+++ main.py
@@ -12,129 +12,163 @@ from keras.models import Sequential
 
 from file_reorganisation import *
 
-create_folders_and_move_image_files()
 
-# Load the data from the subfolders at /images/
-data_dir = pathlib.Path("images")
+def main():
+    # Sort files and get minimum, maximum and average file sizes.
+    # These functions should only be called once.
+    create_folders_and_move_image_files()
+    calculate_min_max_avg_image_size()
 
-# Verify image count
-image_count = len(list(data_dir.glob('*/*.jpg')))
-print(image_count)  # Should be 7390
+    model = None
 
-# Show sample images
-abyssinians = list(data_dir.glob('Abyssinian/*.jpg'))
-# PIL.Image.open(str(abyssinians[1])).show()
+    # If model already exists, load it.
+    if (os.path.exists('model')):
+        model = load_model()
+    # Otherwise, create a new model, train it and save it to a folder.
+    else:
+        model = create_and_train_model()
 
-# Around 200 images in each set, split them into 32-image batches
-batch_size = 32
 
-# Create a dataset
-# Use 20% of images for validation, and 80% for training
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    batch_size=batch_size)
+def create_and_train_model():
+    # Load the data from the subfolders at /images/
+    data_dir = pathlib.Path("images")
+    
+    # Verify image count
+    image_count = len(list(data_dir.glob('*/*.jpg')))
+    print(image_count)  # Should be 7390
+    
+    # Show sample images
+    abyssinians = list(data_dir.glob('Abyssinian/*.jpg'))
+    # PIL.Image.open(str(abyssinians[1])).show()
+    
+    # Around 200 images in each set, split them into 32-image batches
+    batch_size = 32
+    
+    # Target size - all images will be resized to this size
+    target_size = (400, 400)
+    input_shape = (target_size[0], target_size[1], 3)
+    
+    # Create a dataset
+    # Use 20% of images for validation, and 80% for training
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=target_size,
+        batch_size=batch_size)
+    
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=target_size,
+        batch_size=batch_size)
+    
+    class_names = train_ds.class_names
+    print(class_names)  # ['Abyssinian', 'Bengal', 'Birman', ...]
+    
+    # Configure the dataset for performance
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    
+    # Standardize the data
+    normalization_layer = layers.Rescaling(1./255)
+    
+    # RGB values will be in [0, 1] range now
+    normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+    image_batch, label_batch = next(iter(normalized_ds))
+    first_image = image_batch[0]
+    print(np.min(first_image), np.max(first_image))  # Prints 0.0 0.9982423
+    
+    # Create the model
+    num_classes = len(class_names)
+    
+    # TODO: ogarnąć to tutaj
+    # Also problem: obrazki mają różne rozmiary -> najlepiej chyba je znormalizować jakoś programowo, bo inaczej są dymy (None; GlobalAveragePooling2D)
+    # https://stackoverflow.com/a/47796091/17816815
+    
+    # Data augumentation - TODO
+    
+    data_augmentation = keras.Sequential(
+        [
+            layers.RandomFlip("horizontal",
+                              input_shape=input_shape),
+            layers.RandomRotation(0.1),
+            layers.RandomZoom(0.1),
+        ]
+    )
+    
+    model = Sequential([
+        data_augmentation,
+        layers.Rescaling(1./255, input_shape=input_shape),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes)
+    ])
+    
+    # Compile the model
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    
+    model.build()
+    
+    # View the model summary
+    model.summary()
+    
+    # Train the model
+    epochs = 15
+    history = model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=epochs
+    )
+    
+    # Create plots showing loss and accuracy of the training/validation sets
+    
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    
+    epochs_range = range(epochs)
+    
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+    
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
 
-val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="validation",
-    seed=123,
-    batch_size=batch_size)
+    # Save the model to a file
+    model.save('model')
+    
+    return model
 
-class_names = train_ds.class_names
-print(class_names)  # ['Abyssinian', 'Bengal', 'Birman', ...]
 
-# Configure the dataset for performance
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+def load_model():
 
-# Standardize the data
-normalization_layer = layers.Rescaling(1./255)
+    loaded_model = keras.models.load_model('model')
+    return loaded_model
 
-# RGB values will be in [0, 1] range now
-normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-image_batch, label_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
-print(np.min(first_image), np.max(first_image))  # Prints 0.0 0.9982423
 
-# Create the model
-num_classes = len(class_names)
-
-# TODO: ogarnąć to tutaj
-# Also problem: obrazki mają różne rozmiary -> najlepiej chyba je znormalizować jakoś programowo, bo inaczej są dymy (None; GlobalAveragePooling2D)
-# Also: dodać obrazki żeby było wszędzie po 200 obrazków
-# https://stackoverflow.com/a/47796091/17816815
-
-# Data augumentation - TODO
-
-data_augmentation = keras.Sequential(
-    [
-        layers.RandomFlip("horizontal",
-                          input_shape=(None, None, 3)),
-        layers.RandomRotation(0.1),
-        layers.RandomZoom(0.1),
-    ]
-)
-
-model = Sequential([
-    data_augmentation,
-    layers.Rescaling(1./255, input_shape=(None, None, 3)),
-    layers.Conv2D(16, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(32, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.GlobalAveragePooling2D(),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(num_classes)
-])
-
-# Compile the model
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-model.build()
-
-# View the model summary
-model.summary()
-
-# Train the model
-epochs = 15
-history = model.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=epochs
-)
-
-# Create plots showing loss and accuracy of the training/validation sets
-
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-epochs_range = range(epochs)
-
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
-
-# Todo: save the model to a file (for now it has to be re-built every time)
+# Function required for the program to run
+if __name__ == "__main__":
+    main()
